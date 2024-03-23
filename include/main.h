be3c24acb031873bf3b16d5ae532f287c4431345
II-Paulus-II <paulus.coding@gmail.com>
2024-03-23 14:05:03 +0000

    rebuilt basics of project with visual studio

---
+++ include/main.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "resource.h"
+#include "framework.h"
+
+#define MAX_LOADSTRING 100
+
+extern HINSTANCE hInst;                                // current instance
+extern WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
+extern WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+
+// Forward declarations of functions included in this code module:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
